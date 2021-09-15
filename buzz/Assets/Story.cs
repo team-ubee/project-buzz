9b5bf1ae306a528bbf96f031f080f12b35b3a865
Miroslav <gavrilov.miroslav@gmail.com>
2021-09-15 22:28:13 +0200

    H

--- buzz/Assets/Story.cs
+++ buzz/Assets/Story.cs
@@ -9,4 +9,17 @@ public class Story : MonoBehaviour
     public AudioClip[] Trailer;
     public AudioClip[] Hardship;
     public AudioClip[] Finish;
+
+    private List<AudioClip> _Clips = new List<AudioClip>();
+
+    public int NextToPlay = 0;
+
+    public void Awake()
+    {
+        foreach(var clip in Intro) { _Clips.Add(clip); }
+        foreach (var clip in Teaser) { _Clips.Add(clip); }
+        foreach (var clip in Trailer) { _Clips.Add(clip); }
+        foreach (var clip in Hardship) { _Clips.Add(clip); }
+        foreach (var clip in Finish) { _Clips.Add(clip); }
+    }
 }
