63be7a30807761c0d64b757456549080344ab12d
Miroslav <gavrilov.miroslav@gmail.com>
2021-09-15 21:32:51 +0200

    qweq

---
+++ buzz/Assets/Story.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Story : MonoBehaviour
+{
+    public AudioClip[] Intro;
+    public AudioClip[] Teaser;
+    public AudioClip[] Trailer;
+    public AudioClip[] Hardship;
+    public AudioClip[] Finish;
+}
